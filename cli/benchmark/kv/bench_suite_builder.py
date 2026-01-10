860d7c2431ecd44abf1e2515fb82e35004694cf7
jingjinduo <jingjinduo@huawei.com>
2026-01-10 17:20:04 +0800

    Add dsbench readme

--- cli/benchmark/kv/bench_suite_builder.py
+++ cli/benchmark/kv/bench_suite_builder.py
@@ -205,7 +205,7 @@ class KVBenchSuiteBuilder:
         self, kv_args: KVArgs, testcase_index: int
     ) -> BenchTestCase:
         args = self.bench_args.args
-        name = f"Bench_n{args.num}_{args.size}_b{args.batch_num}_t{args.thread_num}"
+        name = f"{args.prefix}_n{args.num}_{args.size}_b{args.batch_num}_t{args.thread_num}"
         testcase = KVBenchTestCase(name, self.bench_args, self.handler, testcase_index)
         testcase.add_set_task(kv_args)
         testcase.add_get_task(kv_args)
