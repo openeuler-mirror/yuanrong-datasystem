f2ae49acc3944e83e2c56e4be0c33ceb9af737ec
yaohaolin <yaohaolin@huawei.com>
2026-02-27 11:57:25 +0800

    Revert "client get worker service from etcd"
    This reverts commit e0b1f25fe95e7a0f33c74339d9b193b5d0051fd7.

--- include/datasystem/utils/connection.h
+++ include/datasystem/utils/connection.h
@@ -21,7 +21,6 @@
 #include "datasystem/object/object_enum.h"
 #include "datasystem/utils/sensitive_value.h"
 #include "datasystem/utils/string_view.h"
-#include "datasystem/utils/service_discovery.h"
 
 namespace datasystem {
 struct ConnectOptions {
@@ -46,7 +45,6 @@ struct ConnectOptions {
     bool enableCrossNodeConnection = false;
     bool enableExclusiveConnection = false;
     bool enableRemoteH2D = false;
-    std::shared_ptr<ServiceDiscovery> serviceDiscovery = nullptr;
 };
 }  // namespace datasystem
 
