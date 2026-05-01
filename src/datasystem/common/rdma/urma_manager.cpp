5f8854438ac6cba0a287f9300aab47a885e7c915
OuGongChang <ougongchang@huawei.com>
2026-05-01 16:36:55 +0800

    add urma log

--- src/datasystem/common/rdma/urma_manager.cpp
+++ src/datasystem/common/rdma/urma_manager.cpp
@@ -855,6 +855,9 @@ Status UrmaManager::WaitToFinish(uint64_t requestId, int64_t timeoutMs)
                     << ", src address:" << localUrmaInfo_.localAddress.ToString()
                     << ", target address:" << event->GetRemoteAddress() << ", dataSize:" << event->GetDataSize()
                     << ", cpuid:" << sched_getcpu() << ", status: " << waitRc.ToString()
+                    << ", urma_inflight_wr_count: " << tbbEventMap_.size()
+                    << ", numa affinity srcChipId: " << static_cast<uint32_t>(args.srcChipId)
+                    << ", numa affinity dstChipId: " << static_cast<uint32_t>(args.dstChipId)
                     << ", suggest: " << URMA_ELAPSED_TOTAL_SUGGEST;
     if (waitRc.GetCode() == StatusCode::K_RPC_DEADLINE_EXCEEDED) {
         return Status(K_URMA_WAIT_TIMEOUT,
