5bee0dfaa8028e4790c00850aadd4ca54a4063e9
Xuke0606 <xuke49@huawei.com>
2026-08-06 01:25:44 +0800

    Remove the global namespace for datasystm::flag to avoid conflicts with gflag symbols

--- tests/perf/zmq/zmq_perf_agt_exe.cpp
+++ tests/perf/zmq/zmq_perf_agt_exe.cpp
@@ -44,9 +44,9 @@ void SignalHandler(int signum)
 int main(int argc, char **argv)
 {
     auto pid = getpid();
-    FLAGS_log_filename = datasystem::FormatString("%s.%d", "zmq_perf_agent", pid);
-    FLAGS_v = datasystem::RPC_KEY_LOG_LEVEL;
-    datasystem::Logging::GetInstance()->Start(FLAGS_log_filename, datasystem::LogProcessRole::WORKER);
+    datasystem::FLAGS_log_filename = datasystem::FormatString("%s.%d", "zmq_perf_agent", pid);
+    datasystem::FLAGS_v = datasystem::RPC_KEY_LOG_LEVEL;
+    datasystem::Logging::GetInstance()->Start(datasystem::FLAGS_log_filename, datasystem::LogProcessRole::WORKER);
     datasystem::st::ZmqPerfAgent zmqPerfAgent;
     auto rc = zmqPerfAgent.ProcessArgs(argc, argv);
     if (rc == 0) {
