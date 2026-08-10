3aa2bfe92e42e834e1ba5745973e9e8e2219f1af
OuGongChang <ougongchang@huawei.com>
2026-08-11 03:09:49 +0800

    fix(worker): Get handler 入口 deadline gate + f.wait_until(absolute deadline),避免超过 requestTimeoutMs
    ProcessGetObjectRequest 入口加 CalcRealRemainingTimeUs<=0 fast-fail; GetObjectsFromAnywhereParallelly 的 f.wait() 改 wait_until(absolute deadline) 超时设 abortAllTasks 再等全部 future 结束(防 UAF)。PublishObject 不加 deadline check(master CreateMeta 已提交后 abort 会留孤儿元数据,补 RemoveMeta 会延长时延且有并发冲突)。

---
+++ tests/ut/worker/object_cache/worker_deadline_gate_test.cpp
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Description: UT for deadline gates in Get/Set handler that prevent exceeding requestTimeoutMs.
+ */
+
+#include <thread>
+#include <gtest/gtest.h>
+
+#include "datasystem/common/rpc/timeout_duration.h"
+#include "datasystem/common/util/request_context.h"
+#include "datasystem/common/util/status_helper.h"
+#include "datasystem/utils/status.h"
+
+namespace datasystem {
+namespace test {
+
+class WorkerDeadlineGateTest : public testing::Test {
+protected:
+    void SetUp() override
+    {
+        GetRequestContext()->reqTimeoutDuration.Init(20);
+    }
+    void TearDown() override
+    {
+        GetRequestContext()->reqTimeoutDuration.Init();
+    }
+};
+
+// Verify that CalcRealRemainingTimeUs returns <=0 after the deadline elapses.
+TEST_F(WorkerDeadlineGateTest, DeadlineExceededReturnsNegativeRemaining)
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(25));
+    int64_t remaining = GetRequestContext()->reqTimeoutDuration.CalcRealRemainingTimeUs();
+    EXPECT_LE(remaining, 0);
+}
+
+// Verify that a fresh 20ms deadline has positive remaining.
+TEST_F(WorkerDeadlineGateTest, FreshDeadlineHasPositiveRemaining)
+{
+    int64_t remaining = GetRequestContext()->reqTimeoutDuration.CalcRealRemainingTimeUs();
+    EXPECT_GT(remaining, 0);
+    EXPECT_LE(remaining, 20000);
+}
+
+// Verify that an uninitialized reqTimeoutDuration returns the 60s default.
+TEST_F(WorkerDeadlineGateTest, UninitializedReturnsPositiveDefault)
+{
+    GetRequestContext()->reqTimeoutDuration.Init();
+    int64_t remaining = GetRequestContext()->reqTimeoutDuration.CalcRealRemainingTimeUs();
+    EXPECT_GT(remaining, 0);
+}
+
+}  // namespace test
+}  // namespace datasystem
