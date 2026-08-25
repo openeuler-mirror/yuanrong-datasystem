fc0d3ed62cb61b56d30606358d43e0163b656a0b
ChamberlainJI <jichenglin1@huawei.com>
2026-08-25 11:08:57 +0800

    fix(helm): omit ZMQ server tuning in BRPC mode
    BRPC-only workers do not register zmq_server_io_context. Render the flag only for the legacy ZMQ path while preserving ZMQ deployment compatibility.
    
    Signed-off-by: ChamberlainJI <jichenglin1@huawei.com>

---
+++ k8s/helm_chart/datasystem/tests/test_rpc_args.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import pathlib
+import shutil
+import subprocess
+import unittest
+
+
+CHART = pathlib.Path(__file__).resolve().parents[1]
+HELM = os.environ.get("HELM_BIN") or shutil.which("helm")
+
+
+@unittest.skipUnless(HELM, "helm is required")
+class WorkerRpcArgsTest(unittest.TestCase):
+    def render(self, use_brpc: bool) -> str:
+        return subprocess.run(
+            [
+                HELM,
+                "template",
+                "datasystem",
+                str(CHART),
+                "--set",
+                f"global.rpc.brpc.useBrpc={str(use_brpc).lower()}",
+            ],
+            check=True,
+            capture_output=True,
+            text=True,
+            timeout=30,
+        ).stdout
+
+    def test_brpc_omits_zmq_server_io_context(self):
+        self.assertNotIn("-zmq_server_io_context=", self.render(use_brpc=True))
+
+    def test_zmq_keeps_zmq_server_io_context(self):
+        self.assertIn("-zmq_server_io_context=", self.render(use_brpc=False))
+
+
+if __name__ == "__main__":
+    unittest.main()
